7b2134e123ffbcd41ce3d5abc6711274b5205c18
Jefferson1506 <jeffersondpalacio@unicesar.edu.co>
2023-05-09 00:00:57 -0500

    kk

--- diamante/lib/main.dart
+++ diamante/lib/main.dart
@@ -5,11 +5,14 @@ import 'package:diamante/listaProductos.dart';
 import 'package:diamante/utilidades.dart';
 import 'package:diamante/validar.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  );
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
